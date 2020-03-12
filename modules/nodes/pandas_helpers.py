9632bab0b47f4ade1d5ab5c782630998b36fc80d
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-03-12 16:51:27 +0100

    Add two usefull and basic nodes to rename and to estimate Norm

--- modules/nodes/pandas_helpers.py
+++ modules/nodes/pandas_helpers.py
@@ -38,3 +38,24 @@ class SumColumns(Node):
         if self.i.ready():
             self.o.meta = self.i.meta
             self.o.data = self.i.data.sum(axis=1).to_frame(self._name)
+
+
+class Rename(Node):
+    """ Rename columns with a list
+    """
+    def __init__(self, names):
+        super().__init__()
+        if not isinstance(names, list):
+            raise ValueError('names should be a list')
+        self.names = names
+
+    def update(self):
+        if not self.i.ready():
+            return
+        data = self.i.data
+        if data.shape[1] != len(self.names):
+            self.logger.warning('Received unexpected shape! Expected %d, received %d columns',
+                                len(self.names), data.shape[1])
+            return
+        data.columns = self.names
+        self.o.set(data, data.index)
